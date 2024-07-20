a57c0e0db05320b793e608788bb0d9f377e8471c
Raghu1322 <raghu1322@gmail.com>
2024-07-20 19:10:19 +0530

    RCP game version 1

---
+++ Rock Paper and Scissor game.py
@@ -0,0 +1,51 @@
+##My First python code for playing Rock, Scissor and paper with Computer
+
+import random
+
+UserScore = 0
+ComputerScore = 0
+ComputerList = ["rock","paper","scissor"]
+
+UserConcent = input("Do you want to play game with computer(Yes/No)? ").lower()
+
+if UserConcent != 'yes':
+    print("You opted out game")
+    quit()
+
+else:
+
+    while True:
+        ComputerChoice = random.choice(ComputerList)
+        print("Computer choose: ", ComputerChoice,"!!")
+
+        while True:
+            User_Input = input("Type either Rock, Scissor or paper: ").lower()
+            if User_Input == ComputerChoice:
+                print("You Got It Correct.")
+                UserScore += 1
+                break
+    
+            else:
+                print("Try Again")
+                ComputerScore += 1
+                continue
+    
+        if UserScore > ComputerScore:
+            print("Your Score:", UserScore, "& Computer Score:", ComputerScore, "and you Won!!")
+            UserConcent = input("Do you want to play again? ").lower()
+            if UserConcent != 'yes':
+                print("You opted out game")
+                quit()
+
+            else:
+                continue
+        else:
+            print("Your Score:", UserScore, "& Computer Score:", ComputerScore, "and you Lost!!")
+            UserConcent = input("Do you want to play again? ").lower()
+            if UserConcent != 'yes':
+                print("You opted out game")
+                quit()
+
+            else:
+                continue
+
